c63a632534dfdec484b2ac1286c380592e3771ef
Hrushi4151 <108774635+Hrushi4151@users.noreply.github.com>
2024-07-01 03:10:49 -0700

    Update route.js

--- src/app/api/getadmins/route.js
+++ src/app/api/getadmins/route.js
@@ -1,25 +1,19 @@
 import Admin from "../../../models/Admin";
-import connectDB from '../../../utils/mongoose'
-
+import connectDB from '../../../utils/mongoose';
 import { NextResponse } from 'next/server';
 
-export async function GET(req,res) {
-
+export async function GET() {
     try {
-        connectDB();
+        await connectDB(); // Ensure this returns a promise that resolves when the connection is established
         
-        const admins=await Admin.find();
-        console.log(admins)
-        if(admins){
-            return NextResponse.json({admins:admins, status: 200 }); 
-        }else{
-            return NextResponse.json({message:"No Admins Found", status: 201 }); 
-
+        const admins = await Admin.find();
+        if (admins) {
+            return NextResponse.json({ admins: admins, status: 200 });
+        } else {
+            return NextResponse.json({ message: "No Admins Found", status: 201 });
         }
-
     } catch (error) {
-        console.log(error)
-        return NextResponse.json("database error occured", { status: 400 });
+        console.error(error);
+        return NextResponse.json({ message: "Database error occurred", status: 400 });
     }
-
 }
